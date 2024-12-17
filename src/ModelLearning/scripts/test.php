ba50104be6aa79ec6ac611561ee315cf372998ba
grintzdel <mathis.oudin@etu.uca.fr>
2024-12-17 19:36:20 +0100

    :hammer: Updated Model scripts

--- src/ModelLearning/scripts/test.php
+++ src/ModelLearning/scripts/test.php
@@ -4,23 +4,20 @@ use Spark\ModelLearning\DatasetLoader;
 use Spark\ModelLearning\ModelTester;
 use Spark\ModelLearning\Service\vectorizedService;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
-
-
-$algorithm = $argv[1] ?? 'tree'; // Récupère l'argument de ligne de commande ou utilise 'tree' par défaut
+$algorithm = $argv[1] ?? 'tree';
 
 $loader = new DatasetLoader();
 $tester = new ModelTester($algorithm);
 $vectorizer = new vectorizedService();
 
 echo "Loading testing dataset...\n";
-$testingDataset = $loader->loadDataset(__DIR__ . '/../../image/testing');
+$testingDataset = $loader->loadDataset(__DIR__ . '/../image/testing');
 $vectorizer->vectorizedImage($testingDataset);
 echo "Testing dataset loaded.\n";
 
-// Load the trained model
-$tester->loadModel(__DIR__ . '/../../models/model_' . $algorithm . '.rbx');
+$tester->loadModel(__DIR__ . '/../../public/model_' . $algorithm . '.rbx');
 
 echo "Testing the model...\n";
 $results = $tester->test($testingDataset);
--- src/ModelLearning/scripts/train.php
+++ src/ModelLearning/scripts/train.php
@@ -3,24 +3,22 @@
 use Spark\ModelLearning\DatasetLoader;
 use Spark\ModelLearning\ModelTrainer;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
-
-$algorithm = $argv[1] ?? 'tree'; // Récupère l'argument de ligne de commande ou utilise 'tree' par défaut
+$algorithm = $argv[1] ?? 'tree';
 
 $loader = new DatasetLoader();
 $trainer = new ModelTrainer($algorithm);
 
 echo "Loading training dataset...\n";
-$trainingDataset = $loader->loadDataset(__DIR__ . '/../../image/training');
+$trainingDataset = $loader->loadDataset(__DIR__ . '/../image/training');
 echo "Training dataset loaded.\n";
 
 echo "Training the model...\n";
 $trainer->train($trainingDataset);
 echo "Model trained.\n";
 
-// Create models directory if it doesn't exist
-$modelsDir = __DIR__ . '/../../models';
+$modelsDir = __DIR__ . '/../../public';
 if (!is_dir($modelsDir)) {
     mkdir($modelsDir, 0777, true);
 }
