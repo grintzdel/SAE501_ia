d352637c5484d59be8dbf0777a6a0e9bd18ed5e2
grintzdel <mathis.oudin@etu.uca.fr>
2024-12-17 14:10:15 +0100

    🎉 Init project

---
+++ tests/FirstTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Spark;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    public function testBasicTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
