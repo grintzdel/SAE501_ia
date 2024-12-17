e82b5557628af90769722bcb2cc1e7083c4861f2
grintzdel <mathis.oudin@etu.uca.fr>
2024-12-17 14:29:36 +0100

    :sparkles: Setup Symfony files

---
+++ src/Web/Controller/ContactController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Spark\Web\Controller;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
+    }
+}
---
+++ src/Web/Kernel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Spark\Web;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    public function __construct(string $environment, bool $debug)
+    {
+        parent::__construct($environment, $debug);
+    }
+
+}
